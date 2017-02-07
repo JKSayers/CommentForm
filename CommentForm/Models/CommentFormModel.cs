af5c26c2dfc2b8a7920d9e134122e5b721bba4f0
JKSayers <jsayers1980@gmail.com>
2017-02-07 14:29:44 -0500

    added properties

--- CommentForm/Models/CommentFormModel.cs
+++ CommentForm/Models/CommentFormModel.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CommentForm.Models
 {
     public class CommentFormModel
     {
+        [Key]
+
+        public int ID { get; set; }
+        
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public int Priority { get; set; }
+
+
     }
 }
